702ee87920765cc2ea097b7ac7f76cdc8d10fde1
shtsno24 <shtsno.24@gmail.com>
2019-11-15 17:39:45 +0900

    wip : outcomes are always zero

--- mnist_AXI_Stream.cpp
+++ mnist_AXI_Stream.cpp
@@ -43,94 +43,66 @@ void network(axis &input_data, axis &output_data) {
 		}
 	}
 
-	padding2d_fix16(1, 1, input_0_depth, input_0_height, input_0_width,
-			(int16_t*) input_0_array, Padding2D_0_height, Padding2D_0_width,
-			(int16_t*) Padding2D_0_array);
+	padding2d_fix16(1, 1,
+	input_0_depth, input_0_height, input_0_width, (int16_t*) input_0_array,
+	Padding2D_0_height, Padding2D_0_width, (int16_t*) Padding2D_0_array);
 
-	separable_conv2d_fix16(Padding2D_0_depth, Padding2D_0_height,
-			Padding2D_0_width, (int16_t*) Padding2D_0_array,
-			SeparableConv2D_0_depth, SeparableConv2D_0_height,
-			SeparableConv2D_0_width, (int16_t*) SeparableConv2D_0_array,
-			(int16_t*) SeparableConv2D_0_m_array,
-			(int16_t*) SeparableConv2D_0_b_d, (int16_t*) SeparableConv2D_0_b_p,
-			3, 3, (int16_t*) SeparableConv2D_0_w_d,
-			(int16_t*) SeparableConv2D_0_w_p, 1,
-			fractal_width_SeparableConv2D_0);
+	separable_conv2d_fix16(Padding2D_0_depth, Padding2D_0_height, Padding2D_0_width, (int16_t*) Padding2D_0_array,
+	SeparableConv2D_0_depth, SeparableConv2D_0_height, SeparableConv2D_0_width, (int16_t*) SeparableConv2D_0_array, (int16_t*) SeparableConv2D_0_m_array,
+	(int16_t*) SeparableConv2D_0_b_d, (int16_t*) SeparableConv2D_0_b_p,
+	3, 3, (int16_t*) SeparableConv2D_0_w_d, (int16_t*) SeparableConv2D_0_w_p, 1, fractal_width_SeparableConv2D_0);
 
-	max_pooling2d_fix16(2, SeparableConv2D_0_depth, SeparableConv2D_0_height,
-			SeparableConv2D_0_width, (int16_t*) SeparableConv2D_0_array,
-			MaxPooling2D_0_depth, MaxPooling2D_0_height, MaxPooling2D_0_width,
-			(int16_t*) MaxPooling2D_0_array);
+	max_pooling2d_fix16(2,
+	SeparableConv2D_0_depth, SeparableConv2D_0_height, SeparableConv2D_0_width, (int16_t*) SeparableConv2D_0_array,
+	MaxPooling2D_0_depth, MaxPooling2D_0_height, MaxPooling2D_0_width, (int16_t*) MaxPooling2D_0_array);
 
-	padding2d_fix16(1, 1, MaxPooling2D_0_depth, MaxPooling2D_0_height,
-			MaxPooling2D_0_width, (int16_t*) MaxPooling2D_0_array,
-			Padding2D_1_height, Padding2D_1_width,
-			(int16_t*) Padding2D_1_array);
+	padding2d_fix16(1, 1,
+	MaxPooling2D_0_depth, MaxPooling2D_0_height, MaxPooling2D_0_width, (int16_t*) MaxPooling2D_0_array,
+	Padding2D_1_height, Padding2D_1_width, (int16_t*) Padding2D_1_array);
 
-	separable_conv2d_fix16(Padding2D_1_depth, Padding2D_1_height,
-			Padding2D_1_width, (int16_t*) Padding2D_1_array,
-			SeparableConv2D_1_depth, SeparableConv2D_1_height,
-			SeparableConv2D_1_width, (int16_t*) SeparableConv2D_1_array,
-			(int16_t*) SeparableConv2D_1_m_array,
-			(int16_t*) SeparableConv2D_1_b_d, (int16_t*) SeparableConv2D_1_b_p,
-			3, 3, (int16_t*) SeparableConv2D_1_w_d,
-			(int16_t*) SeparableConv2D_1_w_p, 1,
-			fractal_width_SeparableConv2D_1);				
+	separable_conv2d_fix16(Padding2D_1_depth, Padding2D_1_height, Padding2D_1_width, (int16_t*) Padding2D_1_array,
+	SeparableConv2D_1_depth, SeparableConv2D_1_height, SeparableConv2D_1_width, (int16_t*) SeparableConv2D_1_array, (int16_t*) SeparableConv2D_1_m_array,
+	(int16_t*) SeparableConv2D_1_b_d, (int16_t*) SeparableConv2D_1_b_p,
+	3, 3, (int16_t*) SeparableConv2D_1_w_d, (int16_t*) SeparableConv2D_1_w_p, 1, fractal_width_SeparableConv2D_1);
 
-	padding2d_fix16(1, 1, MaxPooling2D_1_depth, MaxPooling2D_1_height,
-			MaxPooling2D_1_width, (int16_t*) MaxPooling2D_1_array,
-			Padding2D_2_height, Padding2D_2_width,
-			(int16_t*) Padding2D_2_array);
+	max_pooling2d_fix16(2,
+	SeparableConv2D_1_depth, SeparableConv2D_1_height, SeparableConv2D_1_width, (int16_t*) SeparableConv2D_1_array,
+	MaxPooling2D_1_depth, MaxPooling2D_1_height, MaxPooling2D_1_width, (int16_t*) MaxPooling2D_1_array);
 
-	separable_conv2d_fix16(Padding2D_2_depth, Padding2D_2_height,
-			Padding2D_2_width, (int16_t*) Padding2D_2_array,
-			SeparableConv2D_2_depth, SeparableConv2D_2_height,
-			SeparableConv2D_2_width, (int16_t*) SeparableConv2D_2_array,
-			(int16_t*) SeparableConv2D_2_m_array,
-			(int16_t*) SeparableConv2D_2_b_d, (int16_t*) SeparableConv2D_2_b_p,
-			3, 3, (int16_t*) SeparableConv2D_2_w_d,
-			(int16_t*) SeparableConv2D_2_w_p, 1,
-			fractal_width_SeparableConv2D_2);
+	padding2d_fix16(1, 1,
+	MaxPooling2D_1_depth, MaxPooling2D_1_height, MaxPooling2D_1_width, (int16_t*) MaxPooling2D_1_array,
+	Padding2D_2_height, Padding2D_2_width, (int16_t*) Padding2D_2_array);
 
-	up_sampling2d_fix16(2, SeparableConv2D_2_depth, SeparableConv2D_2_height,
-			SeparableConv2D_2_width, (int16_t*) SeparableConv2D_2_array,
-			UpSampling2D_0_depth, UpSampling2D_0_height, UpSampling2D_0_width,
-			(int16_t*) UpSampling2D_0_array);
+	separable_conv2d_fix16(Padding2D_2_depth, Padding2D_2_height, Padding2D_2_width, (int16_t*) Padding2D_2_array,
+	SeparableConv2D_2_depth, SeparableConv2D_2_height, SeparableConv2D_2_width, (int16_t*) SeparableConv2D_2_array, (int16_t*) SeparableConv2D_2_m_array,
+	(int16_t*) SeparableConv2D_2_b_d, (int16_t*) SeparableConv2D_2_b_p,
+	3, 3, (int16_t*) SeparableConv2D_2_w_d, (int16_t*) SeparableConv2D_2_w_p, 1, fractal_width_SeparableConv2D_2);
 
-	padding2d_fix16(1, 1, UpSampling2D_0_depth, UpSampling2D_0_height,
-			UpSampling2D_0_width, (int16_t*) UpSampling2D_0_array,
-			Padding2D_3_height, Padding2D_3_width,
-			(int16_t*) Padding2D_3_array);
+	up_sampling2d_fix16(2,
+	SeparableConv2D_2_depth, SeparableConv2D_2_height, SeparableConv2D_2_width, (int16_t*) SeparableConv2D_2_array,
+	UpSampling2D_0_depth, UpSampling2D_0_height, UpSampling2D_0_width, (int16_t*) UpSampling2D_0_array);
 
-	separable_conv2d_fix16(Padding2D_3_depth, Padding2D_3_height,
-			Padding2D_3_width, (int16_t*) Padding2D_3_array,
-			SeparableConv2D_3_depth, SeparableConv2D_3_height,
-			SeparableConv2D_3_width, (int16_t*) SeparableConv2D_3_array,
-			(int16_t*) SeparableConv2D_3_m_array,
-			(int16_t*) SeparableConv2D_3_b_d, (int16_t*) SeparableConv2D_3_b_p,
-			3, 3, (int16_t*) SeparableConv2D_3_w_d,
-			(int16_t*) SeparableConv2D_3_w_p, 1,
-			fractal_width_SeparableConv2D_3);
+	padding2d_fix16(1, 1,
+	UpSampling2D_0_depth, UpSampling2D_0_height, UpSampling2D_0_width, (int16_t*) UpSampling2D_0_array,
+	Padding2D_3_height, Padding2D_3_width, (int16_t*) Padding2D_3_array);
 
-	up_sampling2d_fix16(2, SeparableConv2D_3_depth, SeparableConv2D_3_height,
-			SeparableConv2D_3_width, (int16_t*) SeparableConv2D_3_array,
-			UpSampling2D_1_depth, UpSampling2D_1_height, UpSampling2D_1_width,
-			(int16_t*) UpSampling2D_1_array);
+	separable_conv2d_fix16(Padding2D_3_depth, Padding2D_3_height, Padding2D_3_width, (int16_t*) Padding2D_3_array,
+	SeparableConv2D_3_depth, SeparableConv2D_3_height, SeparableConv2D_3_width, (int16_t*) SeparableConv2D_3_array, (int16_t*) SeparableConv2D_3_m_array,
+	(int16_t*) SeparableConv2D_3_b_d, (int16_t*) SeparableConv2D_3_b_p,
+	3, 3, (int16_t*) SeparableConv2D_3_w_d, (int16_t*) SeparableConv2D_3_w_p, 1, fractal_width_SeparableConv2D_3);
 
-	padding2d_fix16(1, 1, UpSampling2D_1_depth, UpSampling2D_1_height,
-			UpSampling2D_1_width, (int16_t*) UpSampling2D_1_array,
-			Padding2D_4_height, Padding2D_4_width,
-			(int16_t*) Padding2D_4_array);
+	up_sampling2d_fix16(2,
+	SeparableConv2D_3_depth, SeparableConv2D_3_height, SeparableConv2D_3_width, (int16_t*) SeparableConv2D_3_array,
+	UpSampling2D_1_depth, UpSampling2D_1_height, UpSampling2D_1_width, (int16_t*) UpSampling2D_1_array);
 
-	separable_conv2d_fix16(Padding2D_4_depth, Padding2D_4_height,
-			Padding2D_4_width, (int16_t*) Padding2D_4_array,
-			SeparableConv2D_4_depth, SeparableConv2D_4_height,
-			SeparableConv2D_4_width, (int16_t*) SeparableConv2D_4_array,
-			(int16_t*) SeparableConv2D_4_m_array,
-			(int16_t*) SeparableConv2D_4_b_d, (int16_t*) SeparableConv2D_4_b_p,
-			3, 3, (int16_t*) SeparableConv2D_4_w_d,
-			(int16_t*) SeparableConv2D_4_w_p, 1,
-			fractal_width_SeparableConv2D_4);
+	padding2d_fix16(1, 1,
+	UpSampling2D_1_depth, UpSampling2D_1_height, UpSampling2D_1_width, (int16_t*) UpSampling2D_1_array,
+	Padding2D_4_height, Padding2D_4_width, (int16_t*) Padding2D_4_array);
+
+	separable_conv2d_fix16(Padding2D_4_depth, Padding2D_4_height, Padding2D_4_width, (int16_t*) Padding2D_4_array,
+	SeparableConv2D_4_depth, SeparableConv2D_4_height, SeparableConv2D_4_width, (int16_t*) SeparableConv2D_4_array, (int16_t*) SeparableConv2D_4_m_array,
+	(int16_t*) SeparableConv2D_4_b_d, (int16_t*) SeparableConv2D_4_b_p,
+	3, 3, (int16_t*) SeparableConv2D_4_w_d, (int16_t*) SeparableConv2D_4_w_p, 1, fractal_width_SeparableConv2D_4);
 
 	for (int depth = 0; depth < SeparableConv2D_4_depth; depth++) {
 		for (int height = 0; height < SeparableConv2D_4_height; height++) {
