d0a753909bef709ffccdf65e5140ac5efd85c291
shtsno24 <shtsno.24@gmail.com>
2019-11-29 10:53:04 +0900

    wip : pointwise does not read kernel

--- layers_c/pointwise_conv2d.cpp
+++ layers_c/pointwise_conv2d.cpp
@@ -4,7 +4,7 @@
 uint8_t pointwise_conv2d_fix16(uint16_t input_depth, uint16_t input_height, uint16_t input_width, int16_t* input,
 uint16_t output_depth, uint16_t output_height, uint16_t output_width, int16_t* output,
 const int16_t* bias,
-uint16_t kernel_height, uint16_t kernel_width, const int16_t* kernel,
+uint16_t kernel_height, uint16_t kernel_width, const int16_t kernel[],
 uint8_t relu, uint8_t fractal_width){
     // uint16_t output_hight = (input_shape.height + 2 * pad - kernel_shape.height) / stride + 1;
     // uint16_t output_width = (input_shape.width + 2 * pad - kernel_shape.width) / stride + 1;
