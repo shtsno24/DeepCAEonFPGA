e251478fab58b3733d1b0e6213f58bc0907a6251
shtsno24 <shtsno.24@gmail.com>
2019-11-30 09:42:01 +0900

    update weight array

--- weights_c/SeparableConv2D_0_fix16.h
+++ weights_c/SeparableConv2D_0_fix16.h
@@ -1,6 +1,6 @@
 /*
  * author : shtsno24
- * Date : 2019-11-13 14:12:04.684064
+ * Date : 2019-11-29 22:24:28.972309
  * array_type : int16
  * fractal_width : 14 bit
  * bit_width : 16 bit
@@ -9,7 +9,7 @@
 #pragma once
 #include <stdint.h>
 
-#define data_width_SeparableConv2D_0 16}}
+#define data_width_SeparableConv2D_0 16
 #define fractal_width_SeparableConv2D_0 14
 
 const uint16_t shape_SeparableConv2D_0_w_d[] = {1, 1, 3, 3};
@@ -19,71 +19,8 @@ const int16_t SeparableConv2D_0_w_d[1][1][3][3] =
 { -783,  3015, -3653}}}};
 
 const uint16_t shape_SeparableConv2D_0_w_p[] = {16, 1, 1, 1};
-//const int16_t SeparableConv2D_0_w_p[16][1][1][1] =
-//{{{{-1770}}},
-//
-//{{{ 6639}}},
-//
-//{{{ 8093}}},
-//
-//{{{ -628}}},
-//
-//{{{ 4920}}},
-//
-//{{{ 9618}}},
-//
-//{{{-9253}}},
-//
-//{{{ 1545}}},
-//
-//{{{-10856}}},
-//
-//{{{ 2733}}},
-//
-//{{{ 1194}}},
-//
-//{{{ 6035}}},
-//
-//{{{ 3956}}},
-//
-//{{{-6935}}},
-//
-//{{{ 5414}}},
-//
-//{{{ 6639}}}};
-
 const int16_t SeparableConv2D_0_w_p[16] =
-{-1770,
-
-6639,
-
-8093,
-
--628,
-
-4920,
-
-9618,
-
--9253,
-
-1545,
-
--10856,
-
-2733,
-
-1194,
-
-6035,
-
-3956,
-
--6935,
-
-5414,
-
-6639};
+{-1770,  6639,  8093,  -628,  4920,  9618, -9253,  1545, -10856,  2733,  1194,  6035,  3956, -6935,  5414,  6639};
 
 const uint16_t shape_SeparableConv2D_0_b_d = 1;
 const int16_t SeparableConv2D_0_b_d[1] = {    0};
