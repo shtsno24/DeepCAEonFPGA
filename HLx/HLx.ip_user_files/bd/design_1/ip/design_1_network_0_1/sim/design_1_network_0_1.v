80be5fedeb34e7bd9fbbb8dc48f18d801b49096d
masudalab <masudalab>
2019-12-20 16:50:48 +0900

    change network output

--- HLx/HLx.ip_user_files/bd/design_1/ip/design_1_network_0_1/sim/design_1_network_0_1.v
+++ HLx/HLx.ip_user_files/bd/design_1/ip/design_1_network_0_1/sim/design_1_network_0_1.v
@@ -48,7 +48,7 @@
 
 
 // IP VLNV: xilinx.com:hls:network:1.0
-// IP Revision: 1912201401
+// IP Revision: 1912201441
 
 `timescale 1ns/1ps
 
--- HLx/HLx.ip_user_files/sim_scripts/design_1/xcelium/run.f
+++ HLx/HLx.ip_user_files/sim_scripts/design_1/xcelium/run.f
@@ -66,11 +66,10 @@
   "../../../bd/design_1/ip/design_1_rst_ps7_0_100M_0/sim/design_1_rst_ps7_0_100M_0.vhd" \
 -endlib
 -makelib xcelium_lib/xil_defaultlib \
-  "../../../../HLx.srcs/sources_1/bd/design_1/ipshared/85a1/hdl/verilog/fifo_w16_d784_A.v" \
-  "../../../../HLx.srcs/sources_1/bd/design_1/ipshared/85a1/hdl/verilog/network_AXILiteS_s_axi.v" \
-  "../../../../HLx.srcs/sources_1/bd/design_1/ipshared/85a1/hdl/verilog/network_MemBank_B.v" \
-  "../../../../HLx.srcs/sources_1/bd/design_1/ipshared/85a1/hdl/verilog/network_MemBank_Out.v" \
-  "../../../../HLx.srcs/sources_1/bd/design_1/ipshared/85a1/hdl/verilog/network.v" \
+  "../../../../HLx.srcs/sources_1/bd/design_1/ipshared/febb/hdl/verilog/network_AXILiteS_s_axi.v" \
+  "../../../../HLx.srcs/sources_1/bd/design_1/ipshared/febb/hdl/verilog/network_MemBank_B.v" \
+  "../../../../HLx.srcs/sources_1/bd/design_1/ipshared/febb/hdl/verilog/network_MemBank_Out.v" \
+  "../../../../HLx.srcs/sources_1/bd/design_1/ipshared/febb/hdl/verilog/network.v" \
   "../../../bd/design_1/ip/design_1_network_0_1/sim/design_1_network_0_1.v" \
 -endlib
 -makelib xcelium_lib/lib_pkg_v1_0_2 \
