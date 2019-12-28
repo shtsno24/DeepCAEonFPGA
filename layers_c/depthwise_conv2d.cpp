f3a34ae3f5107dd001891b2899092c17508cc02c
shtsno24 <shtsno.24@gmail.com>
2019-12-28 23:57:18 +0900

    HLSの配列インデックスの処理にバグがあるかも

--- layers_c/depthwise_conv2d.cpp
+++ layers_c/depthwise_conv2d.cpp
@@ -14,10 +14,24 @@ uint8_t depthwise_conv2d_fix16(uint16_t input_depth, uint16_t input_height,
 	// stride is fixed to 1
 	// dilation rate *must* be (1, 1)
 	int32_t buffer;
+	int16_t w00, w01, w02, w10, w11, w12, w20, w21, w22;
 
 #pragma HLS array_partition variable=kernel
 
 	for (uint16_t out_d = 0; out_d < output_depth; out_d++) {
+
+		w00 = kernel[out_d * kernel_height * kernel_width];
+		w01 = kernel[out_d * kernel_height * kernel_width + 1];
+		w02 = kernel[out_d * kernel_height * kernel_width + 2];
+
+		w10 = kernel[out_d * kernel_height * kernel_width + 3];
+		w11 = kernel[out_d * kernel_height * kernel_width + 4];
+		w12 = kernel[out_d * kernel_height * kernel_width + 5];
+
+		w20 = kernel[out_d * kernel_height * kernel_width + 6];
+		w21 = kernel[out_d * kernel_height * kernel_width + 7];
+		w22 = kernel[out_d * kernel_height * kernel_width + 8];
+
 		for (uint16_t out_h = 0; out_h < output_height; out_h++) {
 			for (uint16_t out_w = 0; out_w < output_width; out_w++) {
 				buffer = bias[out_d];
@@ -36,59 +50,42 @@ uint8_t depthwise_conv2d_fix16(uint16_t input_depth, uint16_t input_height,
 				int32_t tmp_0 =
 						(((int32_t) input[out_d * input_height * input_width
 								+ (out_h + 0) * input_width + (out_w + 0)]
-								* (int32_t) kernel[(out_d * kernel_height
-										* kernel_width) + (0 * kernel_width) + 0])
-								>> fractal_width);
+								* (int32_t) w00) >> fractal_width);
 				int32_t tmp_1 =
 						(((int32_t) input[out_d * input_height * input_width
 								+ (out_h + 0) * input_width + (out_w + 1)]
-								* (int32_t) kernel[(out_d * kernel_height
-										* kernel_width) + (0 * kernel_width) + 1])
-								>> fractal_width);
+								* (int32_t) w01) >> fractal_width);
 				int32_t tmp_2 =
 						(((int32_t) input[out_d * input_height * input_width
 								+ (out_h + 0) * input_width + (out_w + 2)]
-								* (int32_t) kernel[(out_d * kernel_height
-										* kernel_width) + (0 * kernel_width) + 2])
-								>> fractal_width);
+								* (int32_t) w02) >> fractal_width);
 
 				int32_t tmp_3 =
 						(((int32_t) input[out_d * input_height * input_width
 								+ (out_h + 1) * input_width + (out_w + 0)]
-								* (int32_t) kernel[(out_d * kernel_height
-										* kernel_width) + (1 * kernel_width) + 0])
-								>> fractal_width);
+								* (int32_t) w10) >> fractal_width);
 				int32_t tmp_4 =
 						(((int32_t) input[out_d * input_height * input_width
 								+ (out_h + 1) * input_width + (out_w + 1)]
-								* (int32_t) kernel[(out_d * kernel_height
-										* kernel_width) + (1 * kernel_width) + 1])
-								>> fractal_width);
+								* (int32_t) w11) >> fractal_width);
 				int32_t tmp_5 =
 						(((int32_t) input[out_d * input_height * input_width
 								+ (out_h + 1) * input_width + (out_w + 2)]
-								* (int32_t) kernel[(out_d * kernel_height
-										* kernel_width) + (1 * kernel_width) + 2])
-								>> fractal_width);
+								* (int32_t) w12) >> fractal_width);
 
 				int32_t tmp_6 =
 						(((int32_t) input[out_d * input_height * input_width
 								+ (out_h + 2) * input_width + (out_w + 0)]
-								* (int32_t) kernel[(out_d * kernel_height
-										* kernel_width) + (2 * kernel_width) + 0])
-								>> fractal_width);
+								* (int32_t) w20) >> fractal_width);
 				int32_t tmp_7 =
 						(((int32_t) input[out_d * input_height * input_width
 								+ (out_h + 2) * input_width + (out_w + 1)]
-								* (int32_t) kernel[(out_d * kernel_height
-										* kernel_width) + (2 * kernel_width) + 1])
-								>> fractal_width);
+								* (int32_t) w21) >> fractal_width);
 				int32_t tmp_8 =
 						(((int32_t) input[out_d * input_height * input_width
 								+ (out_h + 2) * input_width + (out_w + 2)]
-								* (int32_t) kernel[(out_d * kernel_height
-										* kernel_width) + (2 * kernel_width) + 2])
-								>> fractal_width);
+								* (int32_t) w22) >> fractal_width);
+
 				int32_t tmp_10 = tmp_0 + tmp_1;
 				int32_t tmp_11 = tmp_2 + tmp_3;
 				int32_t tmp_12 = tmp_4 + tmp_5;
