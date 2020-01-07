936efefacc0dbee77f1cb795d4459c17c9288313
masudalab <masudalab>
2020-01-07 13:40:04 +0900

    add re-training parameter

--- weights_c/SeparableConv2D_0_fix16.h
+++ weights_c/SeparableConv2D_0_fix16.h
@@ -1,8 +1,8 @@
 /*
  * author : shtsno24
- * Date : 2019-11-29 22:24:28.972309
+ * Date : 2020-01-06 13:05:29.730080
  * array_type : int16
- * fractal_width : 14 bit
+ * fractal_width : 13 bit
  * bit_width : 16 bit
  *
  */
@@ -10,20 +10,19 @@
 #include <stdint.h>
 
 #define data_width_SeparableConv2D_0 16
-#define fractal_width_SeparableConv2D_0 14
+#define fractal_width_SeparableConv2D_0 13
 
 const uint16_t shape_SeparableConv2D_0_w_d[] = {1, 1, 3, 3};
 const int16_t SeparableConv2D_0_w_d[1][1][3][3] =
-{{{{-12554, -12685,  1933},
-{-5322, -7602, -1037},
-{ -783,  3015, -3653}}}};
-
+{{{{  504,  1016,   -25},
+{ 1523,  3821,  1130},
+{  478,  2078,   803}}}};
 
 const uint16_t shape_SeparableConv2D_0_w_p[] = {16, 1, 1, 1};
 const int16_t SeparableConv2D_0_w_p[16] =
-{-1770,  6639,  8093,  -628,  4920,  9618, -9253,  1545, -10856,  2733,  1194,  6035,  3956, -6935,  5414,  6639};
+{ 1530, -3316, -2145, -1851,  4672,  3149,  3768, -4060,  2451,  3116,  4783, -3673, -2333,    25,  2437,   637};
 
 const uint16_t shape_SeparableConv2D_0_b_d = 1;
 const int16_t SeparableConv2D_0_b_d[1] = {    0};
 const uint16_t shape_SeparableConv2D_0_b_p = 16;
-const int16_t SeparableConv2D_0_b_p[16] = { 1542, -1048,  -333,   783,  -788,  -804,   758,  -342,  1083,  2512,   441,     0,  -963,   103,  -697,  -852};
+const int16_t SeparableConv2D_0_b_p[16] = {   42,  -126,  2975,  2544,   -25,  3715,    36,  -324,  5031,     0,   907,   -70,  3224,  -222,  3299,  3923};
